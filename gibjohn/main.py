bbd758a590877d6d4363420b0848c0dccb2c6076
ATDotC <alextran@exe-coll.ac.uk>
2025-10-16 14:14:19 +0100

    hi

--- gibjohn/main.py
+++ gibjohn/main.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about_us')
-def about_us():
-    return render_template('about_us.html')
 
 @app.route('/contact_us')
 def contact_us():
