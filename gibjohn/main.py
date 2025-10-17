4e53092202cb719e646f42eae4dd244282ed8d20
ATDotC <alextran@exe-coll.ac.uk>
2025-10-17 12:00:44 +0100

    hi

--- gibjohn/main.py
+++ gibjohn/main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found():
+    return render_template("404.html"), 404
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -9,19 +14,26 @@ def home():
 
 @app.route('/contact_us')
 def contact_us():
-    return render_template('contact_us.html')
+    # return render_template('contact_us.html')
+    return page_not_found()
+
 
 @app.route('/for_tutors')
 def for_tutors():
-    return render_template('for_tutor.html')
+    # return render_template('for_tutor.html')
+    return page_not_found()
+
 
 @app.route('/pricing')
 def pricing():
-    return render_template('pricing.html')
+    # return render_template('pricing.html')
+    return page_not_found()
+
 
 @app.route('/log_in')
 def log_in():
-    return render_template('log_in.html')
+    return page_not_found()
+# return render_template('log_in.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
